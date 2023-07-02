13094c7b7a0ecbe5fb0cb4dfee5532aa6301ef35
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2023-07-02 17:34:09 +0000

    Formating and amends applied to followers files

--- followers/serializers.py
+++ followers/serializers.py
@@ -4,14 +4,23 @@ from .models import Follower
 
 
 class FollowerSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Follower model
+    Create method handles the unique constraint on 'owner' and 'followed'
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
-    followed_name = serializers.ReadOnlyField()
+    followed_name = serializers.ReadOnlyField(source='followed.username')
 
     class Meta:
-        def create(self, validated_data):
-            try:
-                return super().create(validated_data)
-            except IntegrityError:
-                raise serializers.ValidationError({
-                    'detail': 'possible duplicate'
-                })
+        model = Follower
+        fields = [
+            'id', 'owner', 'created_at', 'followed', 'followed_name'
+        ]
+    
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
--- followers/views.py
+++ followers/views.py
@@ -5,6 +5,12 @@ from .serializers import FollowerSerializer
 
 
 class FollowerList(generics.ListCreateAPIView):
+    """
+    List all followers, i.e. all instances of a user
+    following another user'.
+    Create a follower, i.e. follow a user if logged in.
+    Perform_create: associate the current logged in user with a follower.
+    """
     serializer_class = FollowerSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Follower.objects.all()
@@ -14,7 +20,11 @@ class FollowerList(generics.ListCreateAPIView):
 
 
 class FollowerDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a follower
+    No Update view, as we either follow or unfollow users
+    Destroy a follower, i.e. unfollow someone if owner
+    """    
     serializer_class = FollowerSerializer
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Follower.objects.all()
-    
