8e0835333441ba4c035e9e5e76ec333f0918ade6
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2023-07-04 19:58:36 +0000

    amended ALLOWED_HOSTS

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'ci-drf-api-rest.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'ci-drf-api-rest-197fa5ef099b.herokuapp.com']
 
 
 # Application definition
